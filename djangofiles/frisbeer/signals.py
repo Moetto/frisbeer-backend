20675147438ca065366e75d9d4f3404c8c863f92
Antti Möttönen <antti.moettoenen@gmail.com>
2016-06-14 20:48:38 +0300

    Add rank system

--- djangofiles/frisbeer/signals.py
+++ djangofiles/frisbeer/signals.py
@@ -1,14 +1,34 @@
+from collections import OrderedDict
+
+from django.db.models import F
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
 from server.settings import ELO_K
 from frisbeer.models import *
 
+from scipy.stats import norm, zscore
+
+ranks = ["Klipsu I", "Klipsu II", "Klipsu III", "Klipsu IV", "Klipsu Mestari", "Klipsu Eliitti Mestari",
+         "Kultapossu I", "Kultapossu II", "Kultapossu III", "Kultapossu Mestari", "Mestari Heittäjä I",
+         "Mestari Heittäjä II", "Mestari Heittäjä Eliitti", "Arvostettu Jallu Mestari", "Legendaarinen Nalle",
+         "Legendaarinen Nalle Mestari", "Korkein Ykkösluokan Mestari", "Urheileva Alkoholisti"]
+
+rank_distribution = OrderedDict()
+step = 1 / len(ranks)
+for i in range(18):
+    rank_distribution[norm.ppf(step * i)] = ranks[i]
+
 
 @receiver(m2m_changed, sender=Game.team2.through)
 @receiver(m2m_changed, sender=Game.team1.through)
 @receiver(post_save, sender=Game)
-def update_elo(sender, instance, **kwargs):
+def update_statistics(sender, instance, **kwargs):
+    update_elo(instance)
+    calculate_ranks()
+
+
+def update_elo(instance):
     print("Updating elos (mabby)")
 
     def calculate_elo_change(player, opponent_elo, win):
@@ -18,8 +38,8 @@ def update_elo(sender, instance, **kwargs):
             actual_score = 0
         Ra = player.elo
         Rb = opponent_elo
-        Ea = 1 / (1+10**((Rb-Ra)/400))
-        return ELO_K*(actual_score-Ea)
+        Ea = 1 / (1 + 10 ** ((Rb - Ra) / 400))
+        return ELO_K * (actual_score - Ea)
 
     def calculate_team_elo(team):
         return sum([player.elo for player in team]) / len(team)
@@ -47,3 +67,25 @@ def update_elo(sender, instance, **kwargs):
             player_elo_change += game.team1_score * calculate_elo_change(player, team1_pregame_elo, False)
             player.elo += player_elo_change
             player.save()
+
+
+def calculate_ranks():
+    all_players = Player.objects.all()
+    players = []
+    for player in all_players:
+        if player.team1.filter(team1_score=2).count() \
+                + player.team2.filter(team2_score=2).count() >= 3:
+            players.append(player)
+
+    scores = [player.elo for player in players]
+    z_scores = zscore(scores)
+
+    for i in range(len(players)):
+        player_z_score = z_scores[i]
+        for required_z_score in rank_distribution.keys():
+            if player_z_score > required_z_score:
+                players[i].rank = rank_distribution[required_z_score]
+            else:
+                break
+    for player in players:
+        player.save()
