f5353463d810e7f561ed6851cec33d8d411c7e39
Antti Möttönen <antti.moettoenen@gmail.com>
2017-05-17 18:48:53 +0300

    Create random games in test db setup to get more data for testing

--- setup_test_database.py
+++ setup_test_database.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 
 from django.db import IntegrityError
 
@@ -7,33 +8,32 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'djangofiles'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', "server.settings")
 
 import django
+
 django.setup()
 
 from frisbeer.models import *
 from django.contrib.auth.models import User
 
 Player.objects.all().delete()
-players = []
-for name in ("jsloth", "T3mu", "Runtu", "ivanhoe_", "lepikko", "Paju"):
+players = set()
+for name in (
+        "jsloth", "T3mu", "Runtu", "ivanhoe_", "lepikko", "Paju", "Gertrud", "Meea",
+        "GamesNate", "zibda", "Jellu", "koops", "Himish"):
     player = Player(name=name)
     player.save()
-    players.append(player)
+    players.add(player)
 
-g = Game(name="Testipeli")
-g.save()
-g.team1 = players[0:3]
-g.team2 = players[3:]
-g.team1_score = 2
-g.team2_score = 1
-g.save()
-
-g = Game(name="Testipeli2")
-g.save()
-g.team1 = players[1:4]
-g.team2 = players[0:1] + players[4:]
-g.team1_score = 2
-g.team2_score = 0
-g.save()
+Game.objects.all().delete()
+for i in range(10):
+    g = Game(name="Testipeli{}".format(i))
+    g.save()
+    t1 = set(random.sample(players, 3))
+    g.team1 = t1
+    g.team2 = random.sample(players - t1, 3)
+    g.team1_score = random.randint(0, 2)
+    g.team2_score = 2 if g.team1_score < 2 else random.randint(0, 1)
+    g.approved = random.choice([True, False])
+    g.save()
 
 try:
     admin = User.objects.create_superuser(username="admin", password="adminpassu", email="")
