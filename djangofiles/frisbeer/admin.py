2d59cf100cbc887247475e4e2b7376bff87de9b0
Antti Möttönen <antti.moettoenen@gmail.com>
2017-08-27 11:33:45 +0300

    Add players inline to game in admin

--- djangofiles/frisbeer/admin.py
+++ djangofiles/frisbeer/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from frisbeer.models import *
 
+
+class PlayerInGameInline(admin.TabularInline):
+    model = GamePlayerRelation
+
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [PlayerInGameInline, ]
+
+
 admin.site.register(Player)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Location)
 admin.site.register(Rank)
