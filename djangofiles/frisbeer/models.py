59f3f3a87cf4cc7e36cc04ec28940ab367fff39d
Uula Ranta <uula@ranta.org>
2019-04-21 23:01:18 +0300

    Revert "Set current season as default"
    This reverts commit 0ebfdd1c94931683c5f8b373c4d59c87b081fc05.

--- djangofiles/frisbeer/models.py
+++ djangofiles/frisbeer/models.py
@@ -75,7 +75,7 @@ class Game(models.Model):
                           (PLAYED, "Played"),
                           (APPROVED, "Approved"))
 
-    season = models.ForeignKey(Season, on_delete=models.SET_NULL, null=True, default=Season.current)
+    season = models.ForeignKey(Season, on_delete=models.SET_NULL, null=True)
 
     players = models.ManyToManyField(Player, related_name='games', through='GamePlayerRelation')
     date = models.DateTimeField(default=now)
